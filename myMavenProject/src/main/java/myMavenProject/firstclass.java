4e748b06d471c10a776c941e3e23114dfeae8262
Amit <amit.kumar.nayak.89@gmail.com>
2021-05-17 18:38:03 +0530

    build

---
+++ myMavenProject/src/main/java/myMavenProject/firstclass.java
@@ -0,0 +1,10 @@
+package myMavenProject;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
